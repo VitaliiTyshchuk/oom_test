a16531766ffd07d4eb6604f87dbab1aa6f5617cc
Vitalii Tyshchuk <vitaliy@gg4l.com>
2019-10-14 10:05:05 +0300

    force oom

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,15 +10,37 @@ public class MainTest {
     @Test
     @SuppressWarnings("all")
     public void testMain() {
-        List<Integer> integerList = new ArrayList<>();
-        Random random = new Random();
+
+        List<Integer> integerList1 = new ArrayList<>();
+        List<Integer> integerList2 = new ArrayList<>();
+        List<Integer> integerList3 = new ArrayList<>();
+
+        Random random1 = new Random();
+        Random random2 = new Random();
+        Random random3 = new Random();
+
         int one = 1;
         int two = 1;
+
         while (one == two) {
-            integerList.add(random.nextInt());
+
+            integerList1.add(random1.nextInt());
+            integerList1.add(random2.nextInt());
+            integerList1.add(random3.nextInt());
+
+            integerList2.add(random1.nextInt());
+            integerList2.add(random2.nextInt());
+            integerList2.add(random3.nextInt());
+
+            integerList3.add(random1.nextInt());
+            integerList3.add(random2.nextInt());
+            integerList3.add(random3.nextInt());
+
         }
 
-        Assert.assertTrue("is empty", !integerList.isEmpty());
+        Assert.assertTrue("is empty", !integerList1.isEmpty());
+        Assert.assertTrue("is empty", !integerList2.isEmpty());
+        Assert.assertTrue("is empty", !integerList3.isEmpty());
     }
 
 }
